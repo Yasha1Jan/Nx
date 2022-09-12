af6bb1df7504d0ffb2a618ab647cbf000642e20f
ysharma <yasha.sharma@radancy.com>
2022-09-12 15:56:20 -0400

    new project

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface Todo {
+  title: string;
+}
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [];
+  
+  constructor(private http:HttpClient){
+    this.fetch();
+  }
+  fetch(){
+    this.http.get<Todo[]>('/api/todos').subscribe((t)=>(this.todos=t));
+  }
+
+addTodo(){
+
+  this.http.post('/api/todos', {}).subscribe(()=>{
+    this.fetch();
+  })
+ // this.todos.push({
+   // title: `New todo ${Math.floor(Math.random() * 1000)}`,
+//  });
+}
 }
